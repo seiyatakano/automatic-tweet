07bc0882f627d5cb2abfd79643128f7201c441ed
seiyatakano <takanosonyes@gmail.com>
2021-02-13 15:49:31 +0900

    change tweet time on the tweet.py

--- tweet.py
+++ tweet.py
@@ -41,7 +41,7 @@ def get_hour():
 
 nowHour = get_hour()
 
-if nowHour==7 or nowHour==12 or nowHour==18 or nowHour==21 or nowHour==15:
+if nowHour==7 or nowHour==12 or nowHour==18 or nowHour==21:
     do_tweet()
 elif nowHour==23:
     todayTweetId=[]
