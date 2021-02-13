ed0ac2e52b385f3245c313736fe47d8c2a4b4e9d
seiyatakano <takanosonyes@gmail.com>
2021-02-13 13:30:56 +0900

    fifth commit

--- tweet.py
+++ tweet.py
@@ -6,14 +6,45 @@ import re
 import datetime
 
 
-CK='JW1Tsn0dh8BOWawfTSxQQC1J8'
-CS='62vmqBdu7xj7RZqs6W3FUBcqgNVIjo0LDhGLV8R7lXqKw3lbEF'
-AT='1221050313022615552-yAlmTilEHMtYnvtFtN6uTlPrDM8GQ9'
-AS='xuKSt9zRORtT2BLKG3i25qWGCff9I0jkflM8vhHI41C3p'
+CK='Ow7OUltQgZnufnraoIluGOUOK'
+CS='HmwzBK0idCkH5hisPwGf85w6Uia2oBwuwhoJjkHuzeJGO69dVe'
+AT='1221050313022615552-JU0mUiurYbOipKFH6IIizbyIku4fFv'
+AS='heaBYyxUuEe8tXffsMJVr93BPr7iQ1jp4CkBAor8ME7CN'
 
 # create twitter object
 auth=tweepy.OAuthHandler(CK,CS)
 auth.set_access_token(AT,AS)
 api=tweepy.API(auth)
 
-api.update_status('初めての投稿です！by python')
+todayTweetId=[]
+
+def do_tweet():
+    tweets_df=pd.read_csv('./tweets.csv')
+    tweetId=np.random.randint(0, len(tweets_df), 1)
+    while tweetId in todayTweetId:
+        tweetId=np.random.randint(0,len(tweets_df),1)
+    todayTweetId.append(tweetId)
+    
+    tweet=str(tweets_df.loc[tweetId,'tweets'])
+    tweet=re.sub(' ','',tweet)
+    for i in range(0,3):
+        tweet=re.sub('^[0-9]','',tweet)
+    tweet=re.sub('Name:tweets,dtype:object$','',tweet)
+    tweet=tweet.replace('\\n','\n')
+    api.update_status(tweet)
+    
+def get_hour():
+    now=str(datetime.datetime.now())
+    times=re.split('[-:. ]',now)
+    nowHour=int(times[3])
+    return nowHour
+
+while True:
+    nowHour=get_hour()
+    
+    if nowHour==7 or nowHour==12 or nowHour==18 or nowHour==21 or nowHour==13:
+        do_tweet()
+    elif nowHour==23:
+        todayTweetId=[]
+        
+    time.sleep(3600)
